93fd84669d95b0926eeb2d5e4c36d258eee61613
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-02-05 00:49:26 -0600

    Se agregó transacción a create.php, pero aún no funciona

--- api/requisitions/create.php
+++ api/requisitions/create.php
@@ -25,7 +25,61 @@
     }
 
     if(sizeof($data["errors"]) == 0){
-        //aqui va la transacción
+        for($index = 0; $index < sizeof($cart); $index++){
+            if($cart[$index]->quantity <= 0){
+                array_push( $data["errors"], "No puedes comprar menos de 1 producto de cualquier tipo");
+            }
+            if($cart[$index]->product->quantity < $cart[$index]->quantity){
+                array_push( $data["errors"], "Sólo se cuenta con " . $cart[$index]->product->quantity ." unidades de " . $cart[$index]->product->name . " en inventario" );
+            }
+        }
+    }
+
+    if(sizeof($data["errors"]) == 0){
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //Crear cliente
+        $sql = "INSERT INTO clients (name, birth, email, phone, password) VALUES (?, '1985-11-7', 'test@test.com', '2221548621','secret');";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($client_name));
+        //Obtener id del cliente recien insertado
+        $sql = "SELECT LAST_INSERT_ID();";
+        $q-> $pdo->prepare($sql);
+        $client_id = $q->execute();
+        //Crear carrito
+        $sql = "INSERT INTO carts (client_id) VALUES (?);";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($client_id));
+        //Obtener id del carrito recien insertado
+        $sql = "SELECT LAST_INSERT_ID();";
+        $q-> $pdo->prepare($sql);
+        $cart_id = $q->execute();
+        //Insertar productos
+        for($index = 0; $index < sizeof($cart); $index++){
+            $sql = "INSERT INTO cart_product (product_id, cart_id, quantity) VALUES (?, ?, ?);";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($cart[index]->product->id, $cart_id, $cart[index]->quantity));
+        }
+        //Crear requisicion
+        $sql = "INSERT INTO requisitions(created_at, cart_id, client_id, delivery_id, payment_type_id, till_id) VALUES (now(), ?, ?, 1, 1, 3);";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($cart_id, $client_id));
+        //Obtener id de la requisicion recien insertada
+        $sql = "SELECT LAST_INSERT_ID();";
+        $q-> $pdo->prepare($sql);
+        $requisition_id = $q->execute();
+        //Actualizar estado de requisición
+        $sql = "INSERT INTO requisition_status(requisition_id, status_id, created_at) VALUES (?, 1, now());";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($requisition_id));
+        //Disminuir stock
+        for($index = 0; $index < sizeof(cart); $index++){
+            $newStock = $cart[index]->product->quantity - $cart[index]->quantity;
+            $sql = "UPDATE branch_product SET quantity = ? WHERE branch_id = 2 AND product_id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($newStock, $cart[index]->product->id));
+        }
+        Database::disconnect();
     }
 
     echo json_encode($data);
